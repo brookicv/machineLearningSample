443754cd91c80804233817d9e643ed70fa143119
Brook <brookicv@gmail.com>
2018-09-06 18:33:09 +0800

    Second commit

---
+++ knn.py
@@ -0,0 +1,22 @@
+
+# -*- coding:utf-8 -*-
+
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from pyimagesearch.preprocessing import simplePreprocessor
+from pyimagesearch.datasets import simpleDatasetLoader
+from imutils import paths
+import argparse
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-d","--dataset",required=True,help="Path to input dataset")
+ap.add_argument("-k","--neighbors",type=int,default=1,help="# of nearest neighbors for classification")
+ap.add_argument("-j","--jobs",type=int,default=1,help="# of jobs for k-NN distance(-1 uses all available cores)")
+
+args = vars(ap.parse_args())
+
+# grab the list of images that we'll be describing
+print("[INFO] loading images...")
+imagePaths = list(paths.list_images(args["dataset"]))
