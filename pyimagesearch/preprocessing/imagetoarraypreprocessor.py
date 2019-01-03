adbbd51c6c88d13addfd68de2d7ee3cffb7dedfd
brookicv <lovelq522@qq.com>
2019-01-04 01:38:10 +0800

    Change some codes

--- pyimagesearch/preprocessing/imagetoarraypreprocessor.py
+++ pyimagesearch/preprocessing/imagetoarraypreprocessor.py
@@ -5,5 +5,5 @@ class ImageToArrayPreprocessor:
     def __init__(self,dataFormat=None):
         self.dataFormat = dataFormat
 
-    def preprocess(self,image):
+    def process(self,image):
         return img_to_array(image,data_format=self.dataFormat)
