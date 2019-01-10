cac1f6230c0eca8f79968e95cbeccd52cd75deb2
brook <brookicv@gmail.com>
2019-01-10 18:06:24 +0800

    handpose

---
+++ handposeTest.py
@@ -0,0 +1,58 @@
+
+import cv2
+from keras.preprocessing.image import img_to_array
+from keras.models import load_model
+import numpy as np 
+import argparse
+import imutils
+import pickle
+
+
+
+model = load_model("minivgg-handpose.tf")
+lb = pickle.loads(open("handpose.hd","rb").read())
+## Grab camera input
+cap = cv2.VideoCapture(0)
+cv2.namedWindow('Original', cv2.WINDOW_NORMAL)
+
+# set rt size as 640x480
+ret = cap.set(3,640)
+ret = cap.set(4,480)
+
+x0 = 400
+y0 = 200
+height = 240
+width = 240
+while True:
+    ret, frame = cap.read()
+    frame = cv2.flip(frame, 3)
+    frame = cv2.resize(frame, (640,480))
+
+    cv2.rectangle(frame, (x0,y0),(x0+width,y0+height),(0,255,0),1)
+    roi = frame[y0:y0+height, x0:x0+width]
+
+    
+    cv2.imshow("handpose",roi)
+
+    roi = cv2.resize(roi,(32,32))
+    roi = roi.astype("float") / 255.0
+    roi = img_to_array(roi)
+    roi = np.expand_dims(roi,axis=0)
+        
+
+    ############## Keyboard inputs ##################
+    key = cv2.waitKey(5) & 0xff
+        
+    ## Use Esc key to close the program
+    if key == 27:
+        break
+    
+    if key == ord("g"):
+        preds = model.predict(roi)[0]
+        
+        idx = np.argmax(preds)
+        label = lb.classed_[indx]
+
+        print("predict class:{},probability:{:.4f}".format(lable,preds[idx]))
+    
+    cv2.imshow("Original",frame)
--- handposeTrainModel.py
+++ handposeTrainModel.py
@@ -8,14 +8,18 @@ from pyimagesearch.nn.conv import lenet
 from pyimagesearch.nn.conv import minivggnet
 from imutils import paths
 from keras.optimizers import SGD
+from keras.preprocessing.image import ImageDataGenerator
 from imutils import paths
 import matplotlib.pyplot as plt 
 import numpy as np 
 import argparse
+import pickle
+import random
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-d","--dataset",required=True,help="path to input dataset")
 ap.add_argument("-m","--model",required=True,help="path to output model")
+ap.add_argument("-l","--lablebin",required=True,help="path to output label binarizer")
 
 args = vars(ap.parse_args())
 
@@ -23,10 +27,12 @@ inputWidth = 32
 inputHeight = 32
 inputDepth = 3
 
-trainingEpochs = 100
+trainingEpochs = 50
 
 print("[INFO] loading images...")
-imagePaths = list(paths.list_images(args["dataset"]))
+imagePaths = sorted(list(paths.list_images(args["dataset"])))
+random.seed(42)
+random.shuffle(imagePaths)
 
 sp = simplePreprocessor.SimplePreprocessor(inputWidth,inputHeight)
 iap = imagetoarraypreprocessor.ImageToArrayPreprocessor()
@@ -39,22 +45,35 @@ classNames = sorted(list(set(labels)))
 classes = len(classNames)
 print(classNames)
 
+labels = np.array(labels)
+lb = LabelBinarizer()
+labels = lb.fit_transform(labels)
+
+print(lb.classes_)
+
+
 (trainX,testX,trainY,testY) = train_test_split(data,labels,test_size=0.25,random_state=42)
 
 trainY = LabelBinarizer().fit_transform(trainY)
 testY = LabelBinarizer().fit_transform(testY)
 
+# data augmentation
+aug = ImageDataGenerator(rotation_range=30,width_shift_range=0.1,height_shift_range=0.1,
+    shear_range=0.2,zoom_range=0.2,horizontal_flip=True,fill_mode="nearest")
 
 # initialize the model
 print("[INFO] compiling model ....")
 #model = lenet.LeNet.build(width=32,height=32,depth=1,classes=4)
+opt = SGD(lr=0.01,decay=0.01/40,momentum=0.9,nesterov=True)
 model = minivggnet.MiniVGGNet.build(width=inputWidth,height=inputHeight,depth=inputDepth,classes=classes)
-model.compile(loss="categorical_crossentropy",optimizer="adam",metrics=["accuracy"])
+model.compile(loss="categorical_crossentropy",optimizer=opt,metrics=["accuracy"])
 
 # train
 print("[INFO] training network...")
-H = model.fit(trainX,trainY,validation_data=(testX,testY),batch_size=64,epochs=trainingEpochs,verbose=1)
 
+#H = model.fit(trainX,trainY,validation_data=(testX,testY),batch_size=64,epochs=trainingEpochs,verbose=1)
+H = model.fit_generator(aug.flow(trainX,trainY,batch_size=32),validation_data=(testX,testY),steps_per_epoch=len(trainX) // 32,
+   epochs=trainingEpochs,verbose=1)
 # evaluate the network
 print("[INFO] evaluating network...")
 predictions = model.predict(testX,batch_size=64)
@@ -63,6 +82,13 @@ print(classification_report(testY.argmax(axis=1),predictions.argmax(axis=1),targ
 # save model
 print("[INFO] saving model...")
 model.save(args["model"])
+
+# save hte label binarizer 
+print("[INFO] serializing label binarizer...")
+f = open(args["lablebin"],"wb")
+f.write(pickle.dump(lb))
+f.close()
+
 
 plt.style.use("ggplot")
 plt.figure()
